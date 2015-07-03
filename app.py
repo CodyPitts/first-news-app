81298c28f4b4e6d5a83c443e5e2e3a83e339581b
Cody Pitts <cody.pitts@gmail.com>
2015-07-02 22:20:01 -0700

    Flask app.py and first template

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+def index():
+	template = 'index.html'
+	return render_template(template)
+
+@app.route("/")
+def index():
+	template = 'index.html'
+	return render_template(template)
+
+if __name__ == '__main__':
+	app.run(debug=True, use_reloader=True)
